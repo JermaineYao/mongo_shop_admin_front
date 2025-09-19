27cb756301322d830b0245613de48f7f51c3a7d5
Akina <jermaineyau0806@gmail.com>
2025-09-19 23:15:26 +0800

    查詢條件, 未完成

--- app/layouts/shop-layout.vue
+++ app/layouts/shop-layout.vue
@@ -23,9 +23,48 @@ const themeOverrides = {
 
     // 圓角 / 尺寸 / 內距 / 字級
     borderRadius: '0px',
-    heightMedium: '48px',
+    heightLarge: '48px',
     paddingMedium: '0 15px',
     fontSizeMedium: '14px' // $n
+  },
+  Select: {
+    peers: {
+      InternalSelection: {
+        color: 'transparent',
+        colorActive: 'rgba(0, 0, 0, 0.2)', // focus 背景
+        textColor: 'rgba(218, 201, 166, 0.8)',
+        placeholderColor: 'rgb(138, 138, 138)',
+
+        border: '0px solid rgb(224, 224, 230)',
+        borderActive: '0px solid rgb(224, 224, 230, 0)',
+        boxShadowActive: '0 0 0 0px rgba(0,0,0,0)',
+        borderHover: '0px solid #ff6600',
+        borderFocus: '0px solid #ff0000',
+        boxShadowFocus: '0 0 0 0 rgba(255, 0, 0, 0.25)',
+
+        borderRadius: '0px',
+        heightLarge: '46px',
+        // 單選模式的左右內距（多選是 paddingMultiple）
+        paddingSingle: '0 15px',
+        fontSizeLarge: '14px'
+      },
+      InternalSelectMenu: {
+        /* 面板類：這些鍵名不是全都有，但你可以先放，Naive 會吃得懂的就套用 */
+        color: 'rgba(10,12,26,.96)', // 面板背景
+        borderRadius: '0px',
+        boxShadow: '0 16px 40px rgba(0,0,0,.45)',
+        boxShadowActive: '0 0 0 0px rgba(0,0,0,0)',
+        optionTextColor: 'rgba(218,201,166,.9)',
+        optionTextColorActive: '#daf0e6',
+        optionTextColorHover: 'rgba(218,201,166,1)',
+        optionColorPending: 'rgba(255,255,255,.06)',
+        optionColorActive: 'rgba(0,255,170, 0)',
+        optionHeight: '36px',
+        optionFontSize: '14px',
+        optionPadding: '10px 14px',
+        optionCheckColor: 'rgba(218,201,166,1)'
+      }
+    }
   }
 }
 </script>
@@ -33,7 +72,7 @@ const themeOverrides = {
 <template>
   <div class="shop-layout view">
     <div class="scroll_wrap">
-      <n-config-provider :theme-overrides="themeOverrides">
+      <n-config-provider abstract :theme-overrides="themeOverrides">
         <slot></slot>
       </n-config-provider>
 
--- app/pages/shop/products/index.vue
+++ app/pages/shop/products/index.vue
@@ -1,5 +1,6 @@
 <script>
 import { NInput, NSelect, NPagination, NConfigProvider } from 'naive-ui'
+import gsap from 'gsap'
 </script>
 
 <script setup>
@@ -35,24 +36,163 @@ watch(
     refresh()
   }
 )
+
+// 商品分類
+const categoryList = [
+  {
+    label: '全部',
+    value: '全部'
+  },
+  {
+    label: '碗',
+    value: '0'
+  },
+  {
+    label: '瓶子',
+    value: '1'
+  },
+  {
+    label: '杯子',
+    value: '2'
+  }
+]
+
+// 排序
+const sortOptions = [
+  {
+    label: '無',
+    value: '0'
+  },
+  {
+    label: '上架日期: 新 -> 舊',
+    value: 'a'
+  },
+  {
+    label: '上架日期: 舊 -> 新',
+    value: 'A'
+  },
+  {
+    label: '商品價格: 高 -> 低',
+    value: 'b'
+  },
+  {
+    label: '商品價格: 低 -> 高',
+    value: 'B'
+  }
+]
+
+// 開啟查詢框
+function openSearch() {
+  gsap.to('.search-tab_wrap', {
+    opacity: 0,
+    onComplete: function () {
+      gsap.set('.open', { display: 'none' })
+      gsap.to('.search_container', {
+        left: 0,
+        display: 'flex',
+        opacity: 1,
+        duration: 0.3,
+        onComplete: function () {
+          gsap.to('.close', { opacity: 1, delay: 0.4, duration: 0.2, display: 'flex' })
+        }
+      })
+    }
+  })
+}
+
+// 關閉查詢框
+function closeSearch() {
+  gsap.to('.search-tab_wrap', {
+    opacity: 0,
+    onComplete: function () {
+      gsap.set('.close', { display: 'none' })
+      gsap.to('.search_container', { left: '-100%', display: 'none', opacity: 0, duration: 0.3 })
+      gsap.to('.open', { opacity: 1, delay: 0.1, duration: 0.2, display: 'flex' })
+    }
+  })
+}
 </script>
 
 <template>
   <div class="page">
     <main class="products-list_container">
+      <div class="search-tab_wrap open" @click="openSearch">
+        <span>查詢條件</span>
+
+        <svg id="search-icon" width="45" height="45">
+          <circle cx="11" cy="11" r="1" />
+          <circle cx="22" cy="11" r="1" />
+          <circle cx="33" cy="11" r="1" />
+
+          <circle cx="11" cy="22" r="1" />
+          <circle cx="22" cy="22" r="1" />
+          <circle cx="33" cy="22" r="1" />
+
+          <circle cx="11" cy="33" r="1" />
+          <circle cx="22" cy="33" r="1" />
+          <circle cx="33" cy="33" r="1" />
+        </svg>
+      </div>
+
       <section class="search_container">
-        <article class="search_wrap">
-          <span class="search-item-name">請輸入商品主名稱</span>
+        <div class="search-tab_wrap close" @click="closeSearch">
+          <span>close</span>
 
-          <n-Input size="large" placeholder="請輸入商品主名稱" clearable />
-        </article>
+          <svg id="search-icon" width="45" height="45">
+            <circle cx="11" cy="11" r="1" />
+            <circle cx="22" cy="11" r="1" />
+            <circle cx="33" cy="11" r="1" />
 
-        <article class="search_wrap">
-          <span class="search-item-name">請輸入商品副名稱</span>
+            <circle cx="11" cy="22" r="1" />
+            <circle cx="22" cy="22" r="1" />
+            <circle cx="33" cy="22" r="1" />
 
-          <n-Input size="large" placeholder="請輸入商品副名稱" clearable />
-        </article>
+            <circle cx="11" cy="33" r="1" />
+            <circle cx="22" cy="33" r="1" />
+            <circle cx="33" cy="33" r="1" />
+          </svg>
+        </div>
+
+        <div class="search_wrap">
+          <article class="search-item">
+            <span class="search-item-name">請輸入商品主名稱</span>
+
+            <n-Input size="large" placeholder="" clearable />
+          </article>
+
+          <article class="search-item">
+            <span class="search-item-name">請輸入商品副名稱</span>
+
+            <n-Input size="large" placeholder="" clearable />
+          </article>
+
+          <article class="search-item">
+            <span class="search-item-name">價格大於</span>
+
+            <n-Input size="large" placeholder="" clearable />
+          </article>
+
+          <article class="search-item">
+            <span class="search-item-name">價格小於</span>
+
+            <n-Input size="large" type="number" placeholder="" clearable />
+          </article>
+
+          <article class="search-item">
+            <span class="search-item-name">請選擇商品分類</span>
+
+            <n-select :options="categoryList" placeholder="" size="large" />
+          </article>
+
+          <article class="search-item">
+            <span class="search-item-name">請選擇排序方式</span>
+
+            <n-select :options="sortOptions" placeholder="" size="large" />
+          </article>
+        </div>
       </section>
+
+      <section class="products-list"></section>
     </main>
   </div>
 </template>
